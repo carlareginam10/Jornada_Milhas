1720d8cf51aadd12aed8349831bdee9cf9b074a3
CARLA REGINA MENDES <46799894+carlareginam10@users.noreply.github.com>
2024-04-28 18:24:20 -0300

    atualização header

--- src/app/Shared/header/header.component.ts
+++ src/app/Shared/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { UserService } from 'src/app/Core/services/user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,4 +9,18 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
+  constructor(
+    private userService: UserService,
+    private router : Router
+  ){
+
+  }
+  user$ = this.userService.retornarUser();
+
+  logout(){
+    this.userService.logout();
+    this.router.navigate(['/login'])
+  }
 }
+
+
